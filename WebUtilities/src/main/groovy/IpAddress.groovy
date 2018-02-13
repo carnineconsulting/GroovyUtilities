c533beea70ac9b4853d0975ea38f10cd5d9204c4
Mike Carnine <carnineconsulting@mail.com>
2018-02-13 13:31:43 -0600

    File deletion

--- WebUtilities/src/main/groovy/IpAddress.groovy
+++
@@ -1,8 +0,0 @@
-package main.groovy
-
-import java.net.InetAddress;
-localhost = InetAddress.getLocalHost();
-println "My local machine name | IP address is : " + localhost
-println "Only IP Address : " + localhost.getHostAddress()
-println "ifconfig".execute().text
-
